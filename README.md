d883cd6ee10224a0db6d391d9edb99bcb3d6a864
Terry Chou <tequilas721@gmail.com>
2023-05-15 15:21:01 +0800

    feat: refactor to py3 and switch send message by webhook

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # check-ssl-certificate-expired
-The tool help to check ssl certificate expired and send mail alert 
+The tool help to check ssl certificate expired and send alert by webhook
--- certificate_expired.py
+++ certificate_expired.py
@@ -1,57 +1,86 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from OpenSSL import SSL
-import socket, datetime
-import smtplib
-from email.mime.text import MIMEText
+import datetime
+import json
+import requests
+import socket
+import ssl
+import time
 
-alert_days = 30
-port = 443
-cert_tested = 0
-cur_date = datetime.datetime.utcnow()
-servers = open( "list.txt", "r")
-response=""
 
-def mail(response):
-    from email.mime.text import MIMEText
-    from subprocess import Popen, PIPE
-    msg = MIMEText(response)
-    msg["From"] = "123@123.com"
-    msg["To"] = "456@456.com"
-    msg["Subject"] = "Certificate expired warning !!"
-    p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
-    p.communicate(msg.as_string())
+def webhook(message, max_retries=3, retry_delay=3):
+    retries = 0
 
-for lists in servers:
-    endpoint = lists.strip().split()[0]
-    domain = lists.strip().split()[1]    
-    cdn = lists.strip().split()[2]
+    headers = {
+        "Content-Type": "application/json"
+    }
 
-    context = SSL.Context(SSL.SSLv23_METHOD)
-    sock = SSL.Connection(context, socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    payload = json.dumps(message)
 
-    try:
-        sock.connect( (str(endpoint) , int(port)) )
-        # Send empty to trigger response
-        sock.send("\x00")
-        get_peer_cert=sock.get_peer_certificate()
-        sock.close()
+    while retries < max_retries:
+        try:
+            response = requests.post(url, data=payload, headers=headers)
+            # 檢查請求是否成功，根據實際需求自行判斷
+            if response.status_code == 200:
+                print(response.text)
+                return None
+        except requests.exceptions.RequestException as e:
+            print(f"Error occurred: {e}")
+            retries += 1
+    
+        # 等待指定的延遲時間
+        time.sleep(retry_delay)
 
-        exp_date =  datetime.datetime.strptime(get_peer_cert.get_notAfter(),'%Y%m%d%H%M%SZ')
-        days_to_expire = int((exp_date - cur_date).days)
-        cert_tested = cert_tested + 1
+    return None
 
-        if alert_days > days_to_expire:
-            response = response + "%s certificate %s will expires in %s days \n" % (cdn, domain, days_to_expire)
-        # else:
-        #     response = response + "%s certificate %s is OK \n" % (cdn, domain)
 
-    except:
-        response = "Unable to connect to %s : %s " % (endpoint, port)
+
+if __name__ == '__main__':
+
+    alert_days = 30
+    port = 443
+    server_list = open( "list.txt", "r")
+    url = ""
+
+
+    for lists in server_list:
+        # endpoint = lists.strip().split()[0]
+        domain = lists.strip().split()[0]
+        cdn = lists.strip().split()[1]
+
+        try:
+            # 連線到網站並獲取憑證
+            ctx = ssl.create_default_context()
+            with ctx.wrap_socket(socket.socket(), server_hostname=domain) as s:
+                s.connect((domain, port))
+                cert = s.getpeercert()
+
+            # 解析憑證的有效期
+            cert_expiry_date = datetime.datetime.strptime(cert['notAfter'], '%b %d %H:%M:%S %Y %Z')
+            
+            # 取得現在的日期和時間
+            now = datetime.datetime.now()
+
+            # 計算憑證的過期剩餘天數
+            remaining_days = (cert_expiry_date - now).days
+
+            if remaining_days < alert_days:
+                # message = "%s certificate %s will expires in %s days \n" % (cdn, domain, remaining_days)
+                message = {"msgtype": "text","text": {"content":"%s certificate %s will expires in %s days \n" % (cdn, domain, remaining_days)}}
+            else:
+                # message = "The certificate for %s on %s is still valid for %s days" % (domain, cdn, remaining_days)
+                message = {"msgtype": "text","text": {"content":"The certificate for %s on %s is still valid for %s days" % (domain, cdn, remaining_days)}}
         
-# print response
-if not response:
-    continue
-else:
-    mail(response)
+        except ssl.SSLError as e:
+            print(f"An error occurred while checking the certificate: {e}")
+        
+        except socket.error as e:
+            print(f"An error occurred while connecting to the domain: {e}")
+
+
+        # print(message)
+        if not message:
+            continue
+        else:
+            webhook(message)
